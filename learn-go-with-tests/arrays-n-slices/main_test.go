8bc2c675d21ca5698772c09165c56be25cb90967
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-20 00:07:17 +0600

    add tests on equal compare on two slices

--- learn-go-with-tests/arrays-n-slices/main_test.go
+++ learn-go-with-tests/arrays-n-slices/main_test.go
@@ -1,6 +1,9 @@
 package arraysnslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -16,6 +19,18 @@ func TestSum(t *testing.T) {
 		want := 10
 		assertCorrectMessage(t, got, want)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 6}, []int{3, 4, 5})
+	want := []int{3, 6, 12}
+
+	// Note:- Go does not let you use equality operators with slices.
+	// You could write a function to iterate over each got and want slice and check their values
+	// but for convenience sake, we can use reflect.DeepEqual()
+	if !reflect.DeepEqual(got, want) { // got != want {
+		t.Errorf("got '%v', want '%v'", got, want)
+	}
 }
 
 func assertCorrectMessage(t testing.TB, got, want int) {
