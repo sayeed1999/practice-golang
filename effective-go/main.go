d7097922dda08da25ec001f1ebcdac8477b8ea16
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-19 16:28:06 +0600

    practice go

--- effective-go/main.go
+++ effective-go/main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"bytes"
 	"effective-go/files"
 	namedresultparams "effective-go/named-result-params"
 	"effective-go/ring"
 	"fmt"
+	"sync"
 )
 
 type T struct {
 	Name  string // name of the object
 	Value int    // its value
+}
+
+type SyncBuffer struct {
+	lock   sync.Mutex   // mutex to protect the buffer
+	buffer bytes.Buffer // buffer to hold data
 }
 
 func main() {
@@ -99,4 +106,80 @@ func main() {
 	for i := 0; i < 5; i++ {
 		defer fmt.Printf("%d ", i)
 	}
+
+	// ******** Data allocation ***********
+	// // in go, the new keyword allocates memory and zeroes it.
+	// p := new(SyncBuffer) // type *SyncBuffer
+	// var v SyncBuffer     // type SyncBuffer
+
+	// But some time the zero value isn't good enough and and initializing constructor is necessary.
+
+	// // The expression new(File) and &File{} are equivalent,
+	// f1 := new(File)              // type *File
+	// f2 := &File{}                // type *File
+	// f1 = NewFile(1, "file1.txt") // using the constructor
+
+	// // array, silces, strings
+	// arr := []string{"no error", "Eio", "invalid argument"}
+	// slices := [...]string{"no error", "Eio", "invalid argument"}
+	// map1 := map[string]string{"Enone": "no error", "Eio": "Eio", "Einval": "invalid argument"}
+
+	// Allocate a slice of 100 ints in go
+	p1 := make([]int, 100) // type []int
+	fmt.Println(p1)
+	// Note: make is used to allocate only slices, maps, and channels. And does not return a pointer!
+
+	// Slices hold references to an underlying array, and if you assign one slice to another, both refer to the same array.
+	// A Read function can therefore accept a slice argument rather than a pointer and a count;
+
+	// Two Dimensional Slices in Go!
+	type Transform [3][3]float64 // a 3x3 matrix, an array of arrays
+	type LinesOfText [][]byte    // a slice of slices
+
+	// Because slices are variable-length, it is possible to have each inner slice be a different length.
+	// That can be a common situation, as in our LinesOfText example: each line has an independent length.
+	text := LinesOfText{
+		[]byte("Now is a good time!"),
+		[]byte("for all gophers"),
+		[]byte("to come to the aid of their party."),
+	}
+	fmt.Println(text)
+
+	trn := Transform{
+		{1, 0, 0},
+		{0, 1, 0},
+		{0, 0, 1},
+	}
+	fmt.Println(trn)
+
+}
+
+type File struct {
+	fd   int    // file descriptor
+	name string // name of the file
+
+}
+
+func NewFile(fd int, name string) *File {
+	if fd < 0 {
+		return nil
+	}
+
+	f := new(File) // type *File
+	f.fd = fd
+	f.name = name
+	return f
+}
+
+// There are lots of boilerplate in the above ctor, we can simplify it using a composite literal
+func NewFile2(fd int, name string) *File {
+	if fd < 0 {
+		return nil
+	}
+
+	f := &File{
+		fd:   fd,
+		name: name,
+	}
+	return f // return the pointer to the composite literal
 }
