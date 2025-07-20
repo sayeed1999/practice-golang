efdba21964d7b747044acdfdbbf399e109a27896
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-21 00:12:16 +0600

    fake http server with httptest package

--- learn-go-with-tests/select/racer_test.go
+++ learn-go-with-tests/select/racer_test.go
@@ -1,10 +1,32 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.dev"
+	// slowURL := "http://www.facebook.com"
+	// fastURL := "http://www.quii.dev"
+
+	slowServer := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
+
+	fastServer := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
