385dcd06e96d68605cd3b66f02cfeaabffdffe6e
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-21 00:21:09 +0600

    reduce code duplication

--- learn-go-with-tests/select/racer_test.go
+++ learn-go-with-tests/select/racer_test.go
@@ -20,16 +20,8 @@ func TestRacer(t *testing.T) {
 	// The only difference is we are wrapping it in an httptest.NewServer which makes it easier to use with testing,
 	// as it finds an open port to listen on and then you can close it when you're done with your test.
 
-	slowServer := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(20 * time.Millisecond)
-			w.WriteHeader(http.StatusOK)
-		}))
-
-	fastServer := httptest.NewServer(
-		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			w.WriteHeader(http.StatusOK)
-		}))
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(3 * time.Millisecond)
 
 	defer slowServer.Close()
 	defer fastServer.Close()
@@ -43,4 +35,12 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(delay)
+			w.WriteHeader(http.StatusOK)
+		}))
 }
