c56145a9ee42d979f4739ca8f9eda6920124483f
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-20 22:42:11 +0600

    improve countdown

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -4,12 +4,20 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWord = "Go!"
+const countDownStart = 3
 
 func main() {
 	CountDown(os.Stdout)
 }
 
 func CountDown(out io.Writer) { // bytes.Buffer implements io.Writer
-	fmt.Fprintf(out, "3")
+	for i := countDownStart; i >= 1; i-- {
+		fmt.Fprintln(out, i)
+		time.Sleep(1 * time.Second)
+	}
+	fmt.Fprint(out, finalWord) // Fprintf vs Fprint ??
 }
