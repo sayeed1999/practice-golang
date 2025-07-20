f5969c354ebdb2eb4ca2e35baa18cf3d9f7f50bc
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-21 00:18:42 +0600

    add notes

--- learn-go-with-tests/select/racer_test.go
+++ learn-go-with-tests/select/racer_test.go
@@ -11,6 +11,15 @@ func TestRacer(t *testing.T) {
 	// slowURL := "http://www.facebook.com"
 	// fastURL := "http://www.quii.dev"
 
+	// From the book: -
+	// httptest.NewServer takes an http.HandlerFunc which we are sending in via an anonymous function.
+	// http.HandlerFunc is a type that looks like this: type HandlerFunc func(ResponseWriter, *Request).
+	// All it's really saying is it needs a function that takes a ResponseWriter and a Request,
+	// which is not too surprising for an HTTP server.
+	// There's really no extra magic here, this is also how you would write a real HTTP server in Go.
+	// The only difference is we are wrapping it in an httptest.NewServer which makes it easier to use with testing,
+	// as it finds an open port to listen on and then you can close it when you're done with your test.
+
 	slowServer := httptest.NewServer(
 		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			time.Sleep(20 * time.Millisecond)
