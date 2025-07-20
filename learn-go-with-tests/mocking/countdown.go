5d2a1ee41f090d34f7b8dbcabb0f913ac3426d47
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-20 22:35:06 +0600

    convert it into a working program

--- learn-go-with-tests/mocking/countdown.go
+++ learn-go-with-tests/mocking/countdown.go
@@ -1,9 +1,14 @@
-package countdown
+package main
 
 import (
 	"fmt"
 	"io"
+	"os"
 )
+
+func main() {
+	CountDown(os.Stdout)
+}
 
 func CountDown(out io.Writer) { // bytes.Buffer implements io.Writer
 	fmt.Fprintf(out, "3")
