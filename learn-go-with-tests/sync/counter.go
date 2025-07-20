3217cb79fe974a7f856364c328c333b1dfa18ead
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-21 01:16:14 +0600

    un embed sync.mutex to avoid bugs in future

--- learn-go-with-tests/sync/counter.go
+++ learn-go-with-tests/sync/counter.go
@@ -3,9 +3,9 @@ package counter
 import "sync"
 
 type Counter struct {
-	// mu    sync.Mutex
-	sync.Mutex // embed sync.Mutex into struct
-	value      int
+	mu sync.Mutex
+	// sync.Mutex // embed sync.Mutex into struct DONT DO THIS!
+	value int
 }
 
 func (c *Counter) Value() int {
@@ -13,7 +13,7 @@ func (c *Counter) Value() int {
 }
 
 func (c *Counter) Inc() {
-	c.Lock()
-	defer c.Unlock()
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
