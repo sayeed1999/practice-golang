653304105090ecce468d4531ae85d2a00bca7859
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2025-07-27 23:54:45 +0600

    add more test cases and fix bug

--- learn-go-with-tests/roman-number-kata/main.go
+++ learn-go-with-tests/roman-number-kata/main.go
@@ -2,11 +2,29 @@ package main
 
 import "strings"
 
-func ConvertToRomain(num int) string {
+func ConvertToRoman(num int) string {
 	var result strings.Builder
 
 	for num > 0 {
-		if num >= 50 {
+		if num >= 1000 {
+			result.WriteString("M")
+			num -= 1000
+		} else if num >= 900 {
+			result.WriteString("CM")
+			num -= 900
+		} else if num >= 500 {
+			result.WriteString("D")
+			num -= 500
+		} else if num >= 400 {
+			result.WriteString("CD")
+			num -= 400
+		} else if num >= 100 {
+			result.WriteString("C")
+			num -= 100
+		} else if num >= 90 {
+			result.WriteString("XC")
+			num -= 90
+		} else if num >= 50 {
 			result.WriteString("L")
 			num -= 50
 		} else if num >= 40 {
